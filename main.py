57528182117c03777a083adb3f9abc90cbd7c818
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-11-21 16:57:56 +0000

    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ screen_size = (1000, 1000)
 screen = pygame.display.set_mode(screen_size)
 
 
-handles = [Handle((120, 120, 120), (20, 20)), Handle(pos=(40, 20)), Handle((120, 120, 120), (20, 40)), Handle(pos=(40, 40))]
+handles = [Handle((120, 120, 120), (150, 150)), Handle(pos=(170, 150)), Handle((120, 120, 120), (150, 170)), Handle(pos=(170, 170))]
 
 lerp_handles = [LerpHandle(handles[0], handles[1]),
                 LerpHandle(handles[1], handles[2]),
@@ -16,7 +16,13 @@ lerp2_handles = [LerpHandle(lerp_handles[0], lerp_handles[1], (0, 0, 255)),
 
 lerp3_handle = LerpHandle(lerp2_handles[0], lerp2_handles[1], (255, 255, 255))
 
+pygame.font.init()
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 35)
+text1 = font.render("W : Show permanent line", True, (255, 0, 255))
+text2 = font.render("S : Show derivation animation", True, (255, 0, 255))
+text3 = font.render("+ : Increase derivation detail", True, (255, 0, 255))
+text4 = font.render("- : Decrease derivation detail", True, (255, 0, 255))
 
 doAnimation = False
 sustainedLines = False
@@ -132,6 +138,11 @@ while True:
 
     for h in handles:
         h.draw(screen)
+
+    screen.blit(text1, (0, 0))
+    screen.blit(text2, (0, 30))
+    screen.blit(text3, (0, 60))
+    screen.blit(text4, (0, 90))
 
     pygame.display.update()
 
